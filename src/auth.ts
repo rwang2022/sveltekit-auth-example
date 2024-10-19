b4853120bb77b5877b19da6a7dbfff2ebf06b3d6
rwang2022 <ryanwang601@gmail.com>
2024-10-19 03:34:30 -0400

    initial commit

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from "@auth/sveltekit/providers/github"
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+  trustHost: true,
+  providers: [
+    GitHub,
+  ],
+})
