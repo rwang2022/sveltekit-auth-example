90d0599d94d939de6b3e3507afa7341110526080
rwang2022 <ryanwang601@gmail.com>
2024-10-19 13:39:33 -0400

    added tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
-<script lang="ts">
-  import Header from "$components/header.svelte"
-  import Footer from "$components/footer.svelte"
-</script>
+<script>import "../app.css";
+// @ts-ignore
+import Header from "$components/header.svelte"
+// @ts-ignore
+import Footer from "$components/footer.svelte"</script>
 
 <div class="container">
-  <Header />
-  <slot />
-  <Footer />
+  <Header></Header>
+  <slot></slot>
+  <Footer></Footer>
 </div>
+
 
 <style>
   :global(body) {
